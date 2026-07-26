439f679c594a66f05708d45a0a17846794cc061c
Mark Fathy <markfathy192@gmail.com>
2026-07-26 15:17:55 +0300

    final new update

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -43,7 +43,7 @@ class MyAppState extends State<MyApp> {
                   showIgnore: false,
                   showLater: true,
                   upgrader: Upgrader(
-                    minAppVersion: '1.5.5+32',
+                    minAppVersion: '1.5.5+33',
                     debugDisplayAlways: false,
                     debugLogging: false,
                     languageCode: 'ar',
